52ea609ec653ac567ef77866f1e46e88b041da8a
royalcaster <gabriel.pechstein@schneeberg.km3.de>
2021-07-10 16:48:18 +0200

    10.07.2021 16:48

--- Main.java
+++ Main.java
@@ -6,6 +6,14 @@ public class Main {
 
 //BAUSTELLEN
 
-//Layout und Ausrichtung im Erfassen-Fenster
-//Auswahl - Entfernen-Problem, eventuelle Lösung: keine Auswahl generell möglich, Druck auf entfernen löscht einfach nur letztes Eleme
-    // weitere Idee: standard package, das engezeigt wird, wenn keines da ist? dann muss der array nie 0 werden
+//getWeight in Left funktioniert perfekt, get Weight in Right leider nur bedingt, die grenzen hauen manchmal nicht hin...
+
+
+
+//NÄCHSTE SCHRITTE
+
+// farbige anzeige bei info_dif (extra klasse erbt von info mit switch für schriftfarbe)
+// Methode, die prüft, ob alle Draggables im container liegen (bezogen auf die Grenzen des JPanels)
+// Methode, die prüft, ob alle Draggables NICHT übereinander liegen (sich in irgendeiner form überschneiden) -> papa fragen, ob nötig wegen eventuellem stapeln
+
+
