56fd6712dab67f86dfba310fde358c73ce764088
royalcaster <gabriel.pechstein@schneeberg.km3.de>
2021-07-13 17:04:25 +0200

    13.07.2021 17:04

--- Main.java
+++ Main.java
@@ -4,15 +4,8 @@ public class Main {
     }
 }
 
-//BAUSTELLEN
-
-//getWeight in Left funktioniert perfekt, get Weight in Right leider nur bedingt, die grenzen hauen manchmal nicht hin...
-
-
-
 //NÄCHSTE SCHRITTE
 
-// farbige anzeige bei info_dif (extra klasse erbt von info mit switch für schriftfarbe)
 // Methode, die prüft, ob alle Draggables im container liegen (bezogen auf die Grenzen des JPanels)
 // Methode, die prüft, ob alle Draggables NICHT übereinander liegen (sich in irgendeiner form überschneiden) -> papa fragen, ob nötig wegen eventuellem stapeln
 
